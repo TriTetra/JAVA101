3139aa8f1c79233175b0b5b541bc5bdd6270fee8
Dimoontri <55631365+TriTetra@users.noreply.github.com>
2023-01-20 22:18:26 +0300

    Add files via upload

---
+++ Pratikler_GitHub/src/LoopsAndMethods/Triangle.java
@@ -0,0 +1,27 @@
+package LoopsAndMethods;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int triangle;
+        System.out.print("Enter number : ");
+        triangle = input.nextInt();
+
+
+        for (int i = 1; i<=triangle; i++){
+            for (int k = 1; k<=i; k++){
+                System.out.print("   ");
+            }
+            for (int k = i; k< triangle; k++){
+                System.out.print(" * ");
+            }
+            for (int k = i; k<= triangle; k++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+    }
+}
