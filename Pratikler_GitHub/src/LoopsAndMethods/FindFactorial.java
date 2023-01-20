a31083e6c555429240e2ff9524335502fc3ed5c2
Dimoontri <55631365+TriTetra@users.noreply.github.com>
2023-01-20 14:32:55 +0300

    Add files via upload

---
+++ Pratikler_GitHub/src/LoopsAndMethods/FindFactorial.java
@@ -0,0 +1,17 @@
+package LoopsAndMethods;
+
+import java.util.Scanner;
+
+public class FindFactorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num,total =1;
+        System.out.print("Enter number : ");
+        num = input.nextInt();
+
+        for (int i=1; i<=num; i++){
+            total *=i;
+        }
+        System.out.println(total);
+    }
+}
